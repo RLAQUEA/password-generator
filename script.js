542fb18e62ba1a03efc860e2cb69c74c8b26e3f1
rlaquea <rlaquea14@gmail.com>
2021-06-24 18:02:28 -0400

    Added validation to user input of password length, randomized characters & confirmed character types

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 
 var charSpecial = ["@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".",];
-// Array of numeric characters to be included in password
+// // Array of numeric characters to be included in password
 
 var charNumeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-// Array of lowercase characters to be included in password
+// // Array of lowercase characters to be included in password
 
 var charLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",];
 
@@ -12,27 +12,98 @@ var charLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
 var charUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 
 
+//create function to grab user input
+//create function to randomize array
+//create function that generates password based on user input and randomization 
+function randomShuffle(array) {
+
+//grabs random index from chosen array
+var grabRandInt = Math.floor(Math.random() * array.length);
+var indexValue = array[grabRandInt]; 
+return indexValue;
+}
+
+function userInput() {
+// length of password must be between 8 and 128 characters
+var charPassword = prompt("How many characters would you like your password to have?");
+ 
+//checks if password letter is between 8 and 128 characters 
+if(charPassword < 8 || charPassword > 128) { 
+    alert("Your password must be between 8 and 128 characters") + 
+    confirm("Please enter a number between 8 and 128:") + 
+    prompt("How many characters would you like your password to have?")
+    }
+
+console.log(charPassword);
+// confirms that special characters are in password
+
+var hasSpecialChar = confirm("Click Ok to confirm special characters in your password.");
+console.log(hasSpecialChar);
+
+//confirms that numeric characters in your password
+var hasNumChar = confirm("Click Ok to confirm numeric characters in your password.");
+console.log(hasNumChar);
+
+//confirms uppercase characters in your password
+var hasUpperChar = confirm("Click Ok to confirm uppercase characters in your password.");
+console.log(hasUpperChar);
+
+var hasLowerChar = confirm("Click Ok to confirm lowercase characters in your password.");
+console.log(hasLowerChar);
+
+var inputValue = {
+    inputLength: charPassword, 
+    inputSpecialChar: hasSpecialChar,
+    inputNumChar: hasNumChar,
+    inputLowerChar: hasLowerChar,
+    inputUpperChar: hasUpperChar,
+}
+
+console.log(inputValue);
+return inputValue;
+}
+
+function generatePassword() {
+var grabUserInput = userInput();
+var newRandPass = []
+var possibleChar = []
+
+if(grabUserInput.inputLowerChar === true) {
+    possibleChar = possibleChar.concat(charLowercase);
+    possibleChar.push(randomShuffle(charLowercase));
+}
+
+if(grabUserInput.inputUpperChar === true) {
+    possibleChar = possibleChar.concat(charUppercase);
+    possibleChar.push(randomShuffle(charUppercase));
+}
+if(grabUserInput.inputNumChar === true) {
+    possibleChar = possibleChar.concat(charNumeric);
+    possibleChar.push(randomShuffle(charNumeric));
+}
+if(grabUserInput.inputLowerChar === true) {
+    possibleChar = possibleChar.concat(charSpecial);
+    possibleChar.push(randomShuffle(charSpecial));
+}
+
+for (let i = 0; i < grabUserInput.inputLength; i++) {
+var stagedPass = randomShuffle(possibleChar); 
+newRandPass.push(stagedPass)
+}
+console.log(newRandPass);
+return newRandPass.join("")
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-generatePassword();
 
-// length of password is between 8 and 128 characters
-
-// var p = prompt("How many characters would you like your password to have?");
-
-// if (passwordGen < 8 || > 128) {
-
-//   prompt("Your password must be between 8 and 128 characters")
-// }
 
